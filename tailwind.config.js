2e10ef35faf099437e1d05c1552e14c1571141e7
maxisab <max.sabate@gmail.com>
2024-01-05 17:19:52 -0800

    started work on navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,16 @@ module.exports = {
     },
     extend: {
       colors: {
-        'dark': '#323031'
+        'dark': '#323031',
+        'light': '#50514F',
+        'yellow': '#FFC857',
+        'yellow-l': '#FFE066',
+        'red': "#DB3A34",
+        'red-l': '#F25F5C',
+        'blue': '#084C61',
+        'blue-l': '#247BA0',
+        'green': '#177E89',
+        'green-l': '#70C1B3',
       },
     },
   },
